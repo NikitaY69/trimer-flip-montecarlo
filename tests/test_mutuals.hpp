4ea1567631a60e665d23f42986894c8f281a80f6
Nikita Allaglo <nikita1999.allaglo@gmail.com>
2025-01-19 19:20:02 +0100

    Add mutual header file for all tests (#30)
    It defines N and Size

---
+++ tests/test_mutuals.hpp
@@ -0,0 +1,10 @@
+#ifndef MUTUALS_HPP
+#define MUTUALS_HPP
+#include <cmath>
+#include "globals.hpp"
+
+// Globals that are normally defined in the main file
+int N = 3000;
+double Size = pow(N/density, 1/3.);
+
+#endif // TEST_HELPERS_HPP
